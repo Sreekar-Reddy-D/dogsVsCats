5052b4b1343b80ad0c81300c26c4d701232241ed
Sreekar-Reddy-D <168199024+Sreekar-Reddy-D@users.noreply.github.com>
2025-06-01 00:48:45 +0530

    Update app.py

--- Dogs_and_cats_predictor/app.py
+++ Dogs_and_cats_predictor/app.py
@@ -4,20 +4,18 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from PIL import Image
 
-# App title and description
 st.title("Dog vs Cat Predictor")
 st.write("Upload an image of a dog or a cat and let the model predict!")
 
-# Load your trained CNN model
 model = load_model("model.h5")
 
-# Upload image through Streamlit
+
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file:
-    # Display uploaded image
+
     img = Image.open(uploaded_file)
-    st.image(img, caption='Uploaded Image', use_container_width=True)  # Fixed here
+    st.image(img, caption='Uploaded Image', use_container_width=True)
 
     # Resize to the same size used during training
     img = img.resize((64, 64))  # Match training dimensions
@@ -25,7 +23,6 @@ if uploaded_file:
     img_array = np.expand_dims(img_array, axis=0)  # Shape: (1, 64, 64, 3)
     img_array = img_array / 255.0  # Normalize
 
-    # Make prediction
     prediction = model.predict(img_array)[0][0]
     result = "Dog" if prediction > 0.5 else "Cat"
     st.write(f"### Prediction: {result}")
